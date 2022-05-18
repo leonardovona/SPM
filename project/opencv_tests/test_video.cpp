c297622709293ea4baceb36b17b7b554c5db7272
leonardovona <leonardo.vona@gmail.com>
2022-05-18 16:01:21 +0200

    added smoothing in opencv tests

--- project/opencv_tests/test_video.cpp
+++ project/opencv_tests/test_video.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <chrono>
 #include "utimer.cpp"
+#include <algorithm>
 
 using namespace std;
 using namespace cv;
@@ -30,21 +31,25 @@ Mat greyscale(Mat frame)
 Mat smooth(Mat frame)
 {
   Mat smoothed = Mat::zeros(frame.rows, frame.cols, CV_8U);
-  for (int i = 1; i < frame.rows - 1; i++)
+  for (int i = 0; i < frame.rows; i++)
   {
-    for (int j = 1; j < frame.cols - 1; j++)
+    for (int j = 0; j < frame.cols; j++)
     {
       u_int16_t value = 0;
-      for (int ii = (i - 1); ii <= (i + 1); ii++)
+      int counter = 0;
+
+      for (int ii = max(0, i - 1); ii <= min(i + 1, frame.rows - 1); ii++)
       {
-        for (int jj = (j - 1); jj <= (j + 1); jj++)
+        for (int jj = max(0, j - 1); jj <= min(j + 1, frame.cols - 1); jj++)
         {
           value += frame.at<u_int8_t>(ii, jj);
+          counter++;
         }
       }
-      smoothed.at<u_int8_t>(i, j) = (value / 9);
+      smoothed.at<u_int8_t>(i, j) = (value / counter);
     }
   }
+
   return smoothed;
 }
 
@@ -66,22 +71,20 @@ int main()
     // Obtain fps and frame count by get() method and print
     // You can replace 5 with CAP_PROP_FPS as well, they are enumerations
     int fps = vid_capture.get(5);
-    cout << "Frames per second :" << fps << endl;
+    cout << "Frames per second: " << fps << endl;
 
     // Obtain frame_count using opencv built in frame count reading method
     // You can replace 7 with CAP_PROP_FRAME_COUNT as well, they are enumerations
     int frame_count = vid_capture.get(7);
-    cout << "  Frame count :" << frame_count << endl;
+    cout << "Frame count: " << frame_count << endl;
   }
   {
     utimer u("Sequential smooth and greyscale");
     while (1)
     {
-
       Mat frame;
       // Capture frame-by-frame
       vid_capture >> frame;
-
       // If the frame is empty, break immediately
       if (frame.empty())
         break;
@@ -95,14 +98,14 @@ int main()
         return -1;
       }
 
-      //imshow("Original", frame);
-      //imshow("Greyscaled", greyscaled);
-      imshow("Smoothed", smoothed);
+      // imshow("Original", frame);
+      // imshow("Greyscaled", greyscaled);
+      //imshow("Smoothed", smoothed);
 
       // Press  ESC on keyboard to exit
-      char c = (char)waitKey(25);
-      if (c == 27)
-        break;
+      //char c = (char)waitKey(25);
+      //if (c == 27)
+      //  break;
     }
 
     // cout << frame << endl;
